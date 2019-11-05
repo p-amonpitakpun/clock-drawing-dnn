d155bc1e9d318b140059e7b03263716db41f140e
Anan Methasate <anan_m@me.com>
2019-11-05 12:57:59 +0700

    add evaluate code

---
+++ metric/evaluate.py
@@ -0,0 +1,23 @@
+from sklearn.metrics import *
+
+
+def evaluate(y_true, y_pred, verbose=False):
+    
+    '''
+    This function return default 4 metrics
+    '''
+
+    acc = accuracy_score(y_true, y_pred)
+    rec = recall_score(y_true, y_pred)
+    prec= precision_score(y_true, y_pred)
+    f1  = f1_score(y_true, y_pred)
+    
+    if verbose:
+        print('--------evaluation---------')
+        print('accuracy:', acc)
+        print('recall:', rec)
+        print('precision:', prec)
+        print('f1 score:', f1)
+        print('---------------------------')
+        
+    return acc, rec, prec, f1
